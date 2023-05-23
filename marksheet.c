54104bd83d1770c39645d6e61d0c255ad5999ff9
umangghoghari24 <125755486+umangghoghari24@users.noreply.github.com>
2023-05-23 11:27:54 +0530

    update

---
+++ marksheet.c
@@ -0,0 +1,39 @@
+// gujarati,math,comp,eng,phy;       int , ,'
+                                               
+//gujarati=90                             printf("Gujarati=%d\n",gujarati);
+//math=60 
+//comp=80
+//eng=75
+//phy=60
+//total=??             1+2+34+4;
+//per=??           per=total*100/500.
+
+
+#include<stdio.h>
+int main()
+{
+    int guj,math,comp,eng,phy,total,per;
+    
+    printf("Enter guj mark\n");
+    scanf("%d",&guj);
+    printf("Enter math mark\n");
+    scanf("%d",&math);
+     printf("Enter comp mark\n");
+     scanf("%d",&comp);
+      printf("Enter eng mark\n");
+      scanf("%d",&eng);
+      printf("Enter phy mark\n");
+      scanf("%d",&phy);
+
+    total=guj+math+comp+eng+phy;
+    per=total*100/500;
+  
+     printf("guj=%d\n",guj);
+     printf("math=%d\n",math);
+     printf("comp=%d\n",comp);
+     printf("eng=%d\n",eng);
+     printf("phy=%d\n",phy);
+     printf("**********\n");
+     printf("total:%d\n",total);
+     printf("per=%d\n",per);
+}
